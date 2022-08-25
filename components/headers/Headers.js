499d191747995871c9c04459b24b670221e1f722
minhazpro3 <minhazpro1@gmail.com>
2022-08-26 00:15:37 +0600

    first commit

---
+++ components/headers/Headers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Headers = () => {
+    return (
+        <div>
+            <h3 className='text-5xl' >Text</h3>
+        </div>
+    );
+};
+
+export default Headers;
